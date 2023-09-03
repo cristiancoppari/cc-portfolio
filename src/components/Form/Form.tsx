5a18f94fee5431d3c15a3a7fce000ec1b264cc58
Cristian <cristian.coppari@gmail.com>
2023-09-02 22:53:41 -0300

    add form animation

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,29 +1,64 @@
+import { useRef } from "react";
+import { motion, useInView } from "framer-motion";
+
 import Button from "@/components/Buttons/Button";
 
+const stagger_els_variants = {
+    hidden: {
+        opacity: 0,
+        y: 50,
+    },
+    visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: "spring",
+            staggerChildren: 0.2,
+        },
+    },
+};
+
 const Form = () => {
+    const ref = useRef(null);
+
+    const isInView = useInView(ref, {
+        once: true,
+        amount: 0.5,
+    });
+
     return (
-        <form className="form">
-            <div className="form-input-container">
+        <motion.form
+            ref={ref}
+            variants={stagger_els_variants}
+            initial={"hidden"}
+            animate={isInView ? "visible" : ""}
+            className="form"
+        >
+            <motion.div variants={stagger_els_variants} className="form-input-container">
                 <label htmlFor="name" className="h6">
                     Name
                 </label>
                 <input type="text" id="name" name="name" />
-            </div>
-            <div className="form-input-container">
+            </motion.div>
+
+            <motion.div variants={stagger_els_variants} className="form-input-container">
                 <label htmlFor="email" className="h6">
                     Email
                 </label>
                 <input type="text" id="email" name="email" />
-            </div>
-            <div className="form-input-container">
+            </motion.div>
+
+            <motion.div variants={stagger_els_variants} className="form-input-container">
                 <label htmlFor="message" className="h6">
                     Message
                 </label>
                 <textarea id="message" name="message" cols={30} rows={5}></textarea>
-            </div>
+            </motion.div>
 
-            <Button type="submit" label="Send message" onClick={() => console.log("clicked")} />
-        </form>
+            <motion.div variants={stagger_els_variants}>
+                <Button type="submit" label="Send message" onClick={() => console.log("clicked")} />
+            </motion.div>
+        </motion.form>
     );
 };
 
