eb81dd33c83f25d9ff66fd5869946e7c800fb328
Cristian <cristian.coppari@gmail.com>
2024-01-24 20:54:44 -0300

    feat: add emoji as favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from "next/app";
 
+import Head from "next/head";
 import { Manrope } from "next/font/google";
 
 import "./globals.css";
@@ -10,8 +11,16 @@ const manrope = Manrope({
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <main className={`${manrope.className}`}>
-            <Component {...pageProps} />
-        </main>
+        <>
+            <Head>
+                <link
+                    rel="icon"
+                    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💻</text></svg>"
+                />
+            </Head>
+            <main className={`${manrope.className}`}>
+                <Component {...pageProps} />
+            </main>
+        </>
     );
 }
